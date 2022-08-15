8d31abf66b3ae77acc9c431364f02008bc221a6a
kincerb <dev.bkincer@gmail.com>
2022-08-15 14:51:52 -0400

    [WIP] Trying pyproject.toml.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def sample_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"Request object below:\n{request!s}", content_type="text/plain")
