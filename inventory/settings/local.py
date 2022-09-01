311848641cd6148f449ec7944a3d538f7e2a5abb
kincerb <dev.bkincer@gmail.com>
2022-08-31 22:54:43 -0400

    Updated settings.

--- inventory/settings/local.py
+++ inventory/settings/local.py
@@ -1,11 +1,28 @@
-from .base import *
+from .base import *  # noqa: F403
+
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = True
 
-INSTALLED_APPS += [
+INSTALLED_APPS += [  # noqa: F405
     "debug_toolbar",
 ]
 
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
+
+DB_HOST = get_env_variable("DB_HOST")
+DB_PORT = get_env_variable("DB_PORT")
+DB_NAME = get_env_variable("POSTGRES_DB")
+DB_USER = get_env_variable("POSTGRES_USER")
+DB_PASSWORD = get_env_variable("POSTGRES_PASSWORD")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
+    }
+}
